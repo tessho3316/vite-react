625eba6422c17d9cf4f780a65d701e3391e9f4a8
tessho3316 <tessho.3316@icloud.com>
2025-09-17 15:49:10 +0900

    直し

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const TitlePage = () => {
+  const title: string = "シュミトモ";
+
+  const navigation = useNavigate()
+  const onMovePage = () => {
+    navigation("/Senni");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="title">
+      <div className="title-container">
+        <h1 className="title-text">{title}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="buttons">
+        <button className="login-btn">ログイン</button>
+        <button className="signup-btn">新規登録</button>
+        <button className="start-btn" onClick={onMovePage}>はじめる</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  );
+}
+
+export const Senni= () => {
+
+  return (
+    <div>
+      <h1>
+        遷移しました。
+      </h1>
+    </div>
   )
 }
 
-export default App
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TitlePage />} />
+        <Route path="/Senni" element={<Senni />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
