e007a883d4e490c8e7fd6d92c6190fbb032293a0
koharu-614 <koharuuu8080@gmial.com>
2025-09-19 12:00:19 +0900

    他の人のプロフィールへの遷移を動的処理に

--- src/component/select_people.tsx
+++ src/component/select_people.tsx
@@ -1,87 +1,28 @@
 import { useNavigate } from 'react-router-dom';
-
-
+import { demo_profile } from './demo_profile';
 
 export const SelectPeople = () => {
-    const navigation = useNavigate()
-    const onMoveAProfile = () => {
-    navigation("/A_profile");
-    }
-    const onMoveBProfile = () => {
-        navigation("/B_profile");
-        }
+  const navigate = useNavigate();
 
-    const onMoveCProfile = () => {
-        navigation("/C_profile");
-    }
-    const onMoveDProfile = () => {
-        navigation("/D_profile");
-    }
-        const onMoveEProfile = () => {
-        navigation("/E_profile");
-    }
-        const onMoveFProfile = () => {
-        navigation("/F_profile");
-    }
-        const onMoveGProfile = () => {
-        navigation("/G_profile");
-    }
-        const onMoveHProfile = () => {
-        navigation("/H_profile");
-    }
-        const onMoveIProfile = () => {
-        navigation("/I_profile");
-    }
-        const onMoveJProfile = () => {
-        navigation("/J_profile");
-    }
-        const onMoveKProfile = () => {
-        navigation("/K_profile");
-    }
-        const onMoveLProfile = () => {
-        navigation("/L_profile");
-    }
-        const onMoveMProfile = () => {
-        navigation("/M_profile");
-    }
-        const onMoveNProfile = () => {
-        navigation("/N_profile");
-    } 
-
-    return (
-        <div>
-            <div className="list">
-                    <button className="item" onClick={onMoveAProfile}>太郎<br/>
-                    Ms.GREEN APPLE</button>
-                    <button className="item" onClick={onMoveBProfile}>花子<br/>
-                    あいみょん,米津玄師</button>
-                    <button className="item" onClick={onMoveCProfile}>ねね<br/>
-                    マカロニえんぴつ,go!go!vanillas,サカナクション</button>
-                    <button className="item" onClick={onMoveDProfile}>健太<br/>
-                    back number,Saucy Dog</button>
-                    <button className="item" onClick={onMoveEProfile}>由美<br/>
-                    YOASOBI,Ado,ずっと真夜中でいいのに。</button>
-                    <button className="item" onClick={onMoveFProfile}>大輝<br/>
-                    ONE OK ROCK,[Alexandros],SUPER BEAVER</button>
-                    <button className="item" onClick={onMoveGProfile}>さくら<br/>
-                    back number</button>
-                    <button className="item" onClick={onMoveHProfile}>拓也<br/>
-                    Creepy Nuts,BUMP OF CHICKEN</button>
-                    <button className="item" onClick={onMoveIProfile}>美咲<br/>
-                    aiko,緑黄色社会,Perfume</button>
-                    <button className="item" onClick={onMoveJProfile}>翔太<br/>
-                    RADWIMPS</button>
-                    <button className="item" onClick={onMoveKProfile}>葵<br/>
-                    Official髭男dism,King Gnu</button>
-                    <button className="item" onClick={onMoveLProfile}>蓮<br/>
-                    SEKAI NO OWARI,Vaundy</button>
-                    <button className="item" onClick={onMoveMProfile}>陽菜<br/>
-                    Saucy Dog,My Hair is Bad,クリープハイプ</button>
-                    
-            </div>
-            <div className="back">
-                <button onClick={() => navigation(-1)}>戻る</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className="list">
+        {demo_profile.slice(1).map(profile => (
+          <button
+            key={profile.id}
+            className="item"
+            onClick={() => navigate(`/profile/${profile.id}`)}
+          >
+            {profile.attributes.name}<br/>
+            {[profile.attributes.artist1, profile.attributes.artist2, profile.attributes.artist3]
+              .filter(Boolean)
+              .join(", ")}
+          </button>
+        ))}
+      </div>
+      <div className="back">
+        <button onClick={() => navigate(-1)}>戻る</button>
+      </div>
+    </div>
+  );
+};
